b7abcbca79981aa4f09a0740af59b5bd07376b93
emon12700 <hasan15-5601@diu.edu.bd>
2024-02-19 22:12:19 +0600

    selected seat added in the show list

--- script/Utility.js
+++ script/Utility.js
@@ -1,24 +1,9 @@
-// function setBackgroundColorInKeyboard(elementId) {
-//   const element = document.getElementById(elementId);
-//   element.classList.add("bg-green-500", "text-black");
-// }
-
-// function eventBubble(seatName) {
-//   const tr = document.createElement("tr");
-//   let cls = 'Economy';
-//   tr.innerHTML = `
-//       <td>${seatName}</td>
-//       <td>${cls}</td>
-//       `
-//   return tr;
-// }
-
-function setBackgroundColorInKeyboard(elementId) {
-  const element = document.getElementById(elementId);
-  element.classList.add("bg-green-500", "text-black");
-}
-
-function removeBackgroundColorInKeyboard(elementId) {
-  const element = document.getElementById(elementId);
-  element.classList.remove("bg-green-500", "text-black");
+function eventBubble(seatName) {
+  const tr = document.createElement("tr");
+  let cls = 'Economy';
+  tr.innerHTML = ` 
+      <td>${seatName}</td>
+      <td>${cls}</td>
+      `
+  return tr;
 }
--- script/sricpt.js
+++ script/sricpt.js
@@ -1,19 +1,13 @@
-// function handleSelect(seat) {
-//   let select = document.getElementById();
-//   setBackgroundColorInKeyboard("A1");
-//   let seat = document.getElementById();
-//   let seatName = "A1";
-//   const tr = eventBubble(seatName);
-//   document.getElementById("list-container").appendChild(tr);
-// }
-
-function handleSelect(seatId) {
-    const element = document.getElementById(seatId);
-    if (element.classList.contains("bg-green-500")) {
-        removeBackgroundColorInKeyboard(seatId);
-    } else {
-        setBackgroundColorInKeyboard(seatId);
-    }
+function selected(button) {
+  const element = button.classList;
+  if (element.contains("bg-green-500")) {
+    element.remove("bg-green-500");
+  } else {
+    let sName = document.getElementsByTagName(button.id);
+    let seatName = button.innerText;
+    const tr = eventBubble(seatName);
+    console.log(seatName);
+    document.getElementById("list-container").appendChild(tr);
+    element.add("bg-green-500");
+  }
 }
-
-// document.addEventListener("keyup", handleSelect());
